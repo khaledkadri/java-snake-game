278cdfd8550b9c85d33e7d4fbbeb654b42fbf93f
khaledkadri <30553725+khaledkadri@users.noreply.github.com>
2025-12-30 11:50:02 +0100

    Update Fenetre.java

--- src/Fenetre.java
+++ src/Fenetre.java
@@ -2,13 +2,8 @@
  * Author : KHALED KADRI   
 
  * LinkedIn : https://www.linkedin.com/in/khaled-kadri/
- * License: Creative Commons - Attribution-NonCommercial (CC BY-NC)
+ * License: Apache License 2.0
  * Copyright © 2013 KHALED KADRI. All rights reserved.
- *
- * This work is licensed under the Creative Commons - Attribution-NonCommercial License.
- * You may not use this work for commercial purposes. 
- * You are free to share and adapt the material as long as proper attribution is given.
- * Attribution must include the original author: "KHALED KADRI". https://github.com/khaledkadri
  */
 
 /*
@@ -965,3 +960,4 @@ public class Fenetre extends JFrame implements KeyListener{
 		} catch (IOException e){}
 	}
 }
+
